6a03ad93ff8130fa8bfe722369b1fe9833cdb4b7
Pau Oliva <pau@eslack.org>
2014-02-08 21:06:31 +0100

    increased version to 1.0.8

--- ggpo.py
+++ ggpo.py
@@ -31,7 +31,7 @@ from threading import Event
 from random import randint
 from operator import itemgetter
 
-VERSION = "1.0.7"
+VERSION = "1.0.8"
 
 COMMANDS = ['/challenge', '/cancel', '/accept', '/decline', '/watch', '/whois', '/whowas', '/join', '/list', '/users', '/motd', '/away', '/back', '/clear', '/verbose', '/quit', '/who', '/n', '/debug']
 
